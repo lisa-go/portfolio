adf11c6d0a89c7c4706f42bc9df7c67b4fbba0f3
Lisa <lis-zaa@live.com>
2022-12-30 03:29:26 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from './components/Home';
+import './styles/style.scss';
+
+export default function App() {
+
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet.</h1>
+            
+        </div>
+    )
+}
