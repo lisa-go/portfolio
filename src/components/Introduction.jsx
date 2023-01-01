88fd73126a45f07fb939631f1383950cdd8f765c
Lisa <lis-zaa@live.com>
2022-12-31 22:58:10 -0600

    removed some shapes from intro, made intro page mobile responsive

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -2,38 +2,28 @@ import { VscFileSubmodule } from 'react-icons/vsc';
 import { GrGraphQl } from 'react-icons/gr';
 import { HiOutlineMail } from 'react-icons/hi';
 
-export default function Introduction () {
+export default function Introduction() {
 
     return (
-        <div id="intro">
-            <div id="topshapes">
-                <div className="tri"></div>
-                <div className="dia"></div>
-                <div className="cir"></div>
-                <div className="trap"></div>
-            </div>
-            <div id="botshapes">
-                <div className="hep"></div>
-                <div className="star"></div>
-                <div className="chev"></div>
-                <div className="tri"></div>
-            </div>
-            <div id="introtext">
-                <p className="header">Welcome to Lisa's Portfolio!</p>
-                <p className="subtitle">Click on the buttons below to navigate</p>
-                <div className="btncont">
-                    <button className="projectbtn">
-                        <VscFileSubmodule size={35} />
-                        <span>Projects</span>
-                    </button>
-                    <button className="skillbtn">
-                        <GrGraphQl size={35} />
-                        <span>Skills</span>
-                    </button>
-                    <button className="contactbtn">
-                        <HiOutlineMail size={35} />
-                        <span>Contact</span>
-                    </button>
+        <div id="intro"><a id="top"> </a>
+            <div className="cir">
+                <div id="introtext">
+                    <p className="header">Welcome to Lisa's Portfolio!</p>
+                    <p className="subtitle">Click on the buttons below to navigate</p>
+                    <div className="btncont">
+                        <a className="projectbtn" href="#projects">
+                            <VscFileSubmodule size={35} />
+                            <span>Projects</span>
+                        </a>
+                        <a className="skillbtn" href="#skills">
+                            <GrGraphQl size={35} />
+                            <span>Skills</span>
+                        </a>
+                        <a className="contactbtn" href="#contact">
+                            <HiOutlineMail size={35} />
+                            <span>Contact</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
