22a059e78836f17588232f802a629e76c3889d39
Lisa <lis-zaa@live.com>
2023-01-29 01:13:08 -0600

    fixed windows clone image

--- src/components/Projectslist.js
+++ src/components/Projectslist.js
@@ -2,7 +2,7 @@ export let projectlist = [
     {
         id: 0,
         name: 'windows-clone',
-        image: 'https://camo.githubusercontent.com/196497b3f267e6e51f1105b1078c49f61b0caf62e1d964b8f874eaa0656f3cd0/68747470733a2f2f73332e67696679752e636f6d2f696d616765732f736561726368616e64576561746865722e706e67',
+        image: 'https://i.imgur.com/c7xUmr3.png',
         github: 'https://github.com/KkapKane/ReactTS-windows-clone',
         preview: 'https://kkapkane.github.io/ReactTS-windows-clone/'
     },
