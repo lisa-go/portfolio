a253df9a08093665a6c6ebe05cc86be329153446
Lisa <lis-zaa@live.com>
2023-06-30 12:11:36 -0500

    added ingredient-ally

--- src/components/Projectslist.js
+++ src/components/Projectslist.js
@@ -1,39 +1,40 @@
 export let projectlist = [
   {
     id: 0,
+    name: 'ingredient-ally',
+    image: 'https://i.imgur.com/MHVKMFp.png',
+    github: 'https://github.com/lisa-go/ingredient-ally',
+    preview: 'https://lisa-go.github.io/ingredient-ally/',
+  },
+  {
+    id: 1,
     name: 'restaurant-dashboard',
     image: 'https://i.imgur.com/CT2v96p.png',
     github: 'https://github.com/lisa-go/restaurant-dashboard',
     preview: 'https://lisa-go.github.io/restaurant-dashboard/',
   },
   {
-    id: 1,
+    id: 2,
     name: 'windows-clone',
     image: 'https://i.imgur.com/c7xUmr3.png',
     github: 'https://github.com/KkapKane/ReactTS-windows-clone',
     preview: 'https://kkapkane.github.io/ReactTS-windows-clone/',
   },
   {
-    id: 2,
+    id: 3,
     name: 'twitter-clone',
     image: 'https://i.imgur.com/CbyoGwY.png',
     github: 'https://github.com/lisa-go/twitter-clone',
     preview: 'https://lisa-go.github.io/twitter-clone/',
   },
   {
-    id: 3,
+    id: 4,
     name: 'shopping-cart',
     image: 'https://i.imgur.com/LDhCsNQ.png',
     github: 'https://github.com/lisa-go/shopping-cart',
     preview: 'https://lisa-go.github.io/shopping-cart/',
   },
-  {
-    id: 4,
-    name: 'cv-application',
-    image: 'https://i.imgur.com/uDqhiox.png',
-    github: 'https://github.com/lisa-go/cv-application',
-    preview: 'https://lisa-go.github.io/cv-application/',
-  },
+
   {
     id: 5,
     name: 'etch-a-sketch',
